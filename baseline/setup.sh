1b15f1f224eaf2b70bdfc2c5723f332fc58601db
彭秋 <qiup@phoneyou.net>
2018-03-13 13:58:01 +0800

    checkout stable

--- baseline/setup.sh
+++ baseline/setup.sh
@@ -52,8 +52,17 @@ mkdir /srv/phabricator
 chown PHABRICATOR:wwwgrp-phabricator /srv/phabricator
 cd /srv/phabricator
 sudo -u PHABRICATOR git clone https://www.github.com/phacility/libphutil.git /srv/phabricator/libphutil
+cd libphutil
+git checkout -B stable origin/stable
+cd ..
 sudo -u PHABRICATOR git clone https://www.github.com/phacility/arcanist.git /srv/phabricator/arcanist
+cd arcanist
+git checkout -B stable origin/stable
+cd ..
 sudo -u PHABRICATOR git clone https://www.github.com/phacility/phabricator.git /srv/phabricator/phabricator
+cd phabricator
+git checkout -B stable origin/stable
+cd ..
 sudo -u PHABRICATOR git clone https://www.github.com/PHPOffice/PHPExcel.git /srv/phabricator/PHPExcel
 cd /
 
